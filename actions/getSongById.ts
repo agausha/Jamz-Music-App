4c3c47a09a9db89529353d27e66d2ab3a31db384
Isaac <uniqucode@gmail.com>
2023-06-09 16:37:14 +0100

    Fix a bug

--- actions/getSongById.ts
+++ actions/getSongById.ts
@@ -1,18 +1,17 @@
-import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 import { Song } from "@/types";
 
 const getSongById = async (id: string): Promise<Song> => {
-  const supabase = createServerComponentSupabaseClient({
-    headers: headers,
-    cookies: cookies
+  const supabase = createServerComponentClient({
+    cookies: cookies,
   });
 
   const { data, error } = await supabase
-    .from('songs')
-    .select('*')
-    .eq('id', id)
+    .from("songs")
+    .select("*")
+    .eq("id", id)
     .single();
 
   if (error) {
